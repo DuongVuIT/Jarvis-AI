28d802b3b224273384709b3a6ed43155667af6f3
DuongVuIT <duonghutech26@gmail.com>
2023-09-14 14:33:54 +0700

    commit code

---
+++ App.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View className="flex-1 flex justify-center items-center">
+      <Text>App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
